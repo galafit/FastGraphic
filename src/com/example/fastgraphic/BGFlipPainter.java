1082d668f7cc1d9f0ec28c7063c52fbdf915e53b
Alvw <flashvw@gmail.com>
2012-01-25 12:15:08 +0400

    BGFlipPainter bug was fixed

--- src/com/example/fastgraphic/BGFlipPainter.java
+++ src/com/example/fastgraphic/BGFlipPainter.java
@@ -10,12 +10,15 @@ public class BGFlipPainter implements Painter {
     private boolean colorFlag;
     private Color firstColor = Color.RED;
     private Color secondColor = Color.BLUE;
+    private Color initialColor;
 
     public void paint(Graphics g) {
+        initialColor = g.getColor();
         changeColor(g);
         int width = g.getClipBounds().width;
         int height = g.getClipBounds().height;
         g.fillRect(0,0,width,height);
+        g.setColor(initialColor);
     }
 
     private void changeColor(Graphics g) {
