fbf7fbf78192a1aed6b11b015dbb76bedb7325d8
Alvw <flashvw@gmail.com>
2012-01-25 12:03:42 +0400

    BGFlipPainter was implemented

--- src/com/example/fastgraphic/BGFlipPainter.java
+++ src/com/example/fastgraphic/BGFlipPainter.java
@@ -6,7 +6,25 @@ import java.awt.*;
  *
  */
 public class BGFlipPainter implements Painter {
+
+    private boolean colorFlag;
+    private Color firstColor = Color.RED;
+    private Color secondColor = Color.BLUE;
+
     public void paint(Graphics g) {
-        throw new UnsupportedOperationException("todo");
+        changeColor(g);
+        int width = g.getClipBounds().width;
+        int height = g.getClipBounds().height;
+        g.fillRect(0,0,width,height);
     }
+
+    private void changeColor(Graphics g) {
+        colorFlag = !colorFlag;
+        if(colorFlag){
+            g.setColor(firstColor);
+        } else {
+            g.setColor(secondColor);
+        }
+    }
+
 }
