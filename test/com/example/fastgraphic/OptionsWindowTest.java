d0066da2d6d4842374019264541ebf83482c0235
Galina Troshchenko <galafit@gmail.com>
2012-02-14 19:14:35 +0100

    Class OptionsWindowTest was debugged

--- test/com/example/fastgraphic/OptionsWindowTest.java
+++ test/com/example/fastgraphic/OptionsWindowTest.java
@@ -20,13 +20,12 @@ public class OptionsWindowTest {
         assertEquals(params.getFgColor().getColor(),Color.GREEN);
         assertEquals(params.getFrameRate(), 60);
         assertEquals(params.getFrameShift(),0,5f);
-        assertEquals(params.getGTool(), PaintingAreaName.AWT);
+        assertEquals(params.getApplicationType(), ApplicationType.AWT);
         assertEquals(params.getHeight(),480);
         assertEquals(params.getWidth(),640);
         assertEquals(params.isUseBgFlipPainter(),false);
         assertEquals(params.isUseLinePainter(),false);
         assertEquals(params.isUseSinusPainter(),true);
-        assertEquals(params.isUseSlowPainter(),false);
     }
 
 }
